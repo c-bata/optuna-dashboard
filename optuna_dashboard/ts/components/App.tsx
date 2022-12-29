08359218515c793f413732d9fd9bf22463257396
c-bata <contact@c-bata.link>
2022-12-29 11:34:02 +0900

    update

--- optuna_dashboard/ts/components/App.tsx
+++ optuna_dashboard/ts/components/App.tsx
@@ -1,65 +1,8 @@
-import React, { FC, useMemo, useState, useEffect } from "react"
-import { RecoilRoot } from "recoil"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import { SnackbarProvider } from "notistack"
-import {
-  createTheme,
-  useMediaQuery,
-  ThemeProvider,
-  Box,
-  CssBaseline,
-} from "@mui/material"
+import React, { FC } from "react"
 
-import { StudyDetail } from "./StudyDetail"
-import { StudyList } from "./StudyList"
 
 export const App: FC = () => {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
-  const [colorMode, setColorMode] = useState<"light" | "dark">("light")
-  useEffect(() => {
-    setColorMode(prefersDarkMode ? "dark" : "light")
-  }, [prefersDarkMode])
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: colorMode,
-        },
-      }),
-    [colorMode]
-  )
-  const toggleColorMode = () => {
-    setColorMode(colorMode === "dark" ? "light" : "dark")
-  }
-
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Box
-          sx={{
-            backgroundColor: colorMode === "dark" ? "#121212" : "#ffffff",
-            width: "100%",
-            minHeight: "100vh",
-            paddingBottom: theme.spacing(2),
-          }}
-        >
-          <SnackbarProvider maxSnack={3}>
-            <Router>
-              <Switch>
-                <Route
-                  path={URL_PREFIX + "/studies/:studyId"}
-                  children={<StudyDetail toggleColorMode={toggleColorMode} />}
-                />
-                <Route
-                  path={URL_PREFIX + "/"}
-                  children={<StudyList toggleColorMode={toggleColorMode} />}
-                />
-              </Switch>
-            </Router>
-          </SnackbarProvider>
-        </Box>
-      </ThemeProvider>
-    </RecoilRoot>
+    <p>Hello</p>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,28 @@
-import { defineConfig, splitVendorChunk } from "vite";
+import {defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
-export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, "optuna_dashboard/ts/index.tsx"),
-            name: "optuna-dashboard",
-            fileName: "bundle",
+export default defineConfig(({ command, mode}) => {
+    const env = loadEnv(mode, process.cwd(), 'OPTUNA_')
+    return {
+        //build: {
+        //},
+        //build: {
+        //    lib: {
+        //        entry: resolve(__dirname, "optuna_dashboard/ts/index.tsx"),
+        //        name: "optuna-dashboard",
+        //        fileName: "bundle",
+        //    },
+        //    outDir: resolve(__dirname, "optuna_dashboard/public"),
+        //},
+        define: {
+            API_ENDPOINT: env.API_ENDPOINT || "127.0.0.1:8080",
+            APP_BAR: env.APP_BAR_TITLE || "Optuna Dashboard",
         },
-        outDir: resolve(__dirname, "optuna_dashboard/public"),
-    },
-    plugins: [
-        react(),
-        //splitVendorChunk()
-    ],
+        publicDir: resolve(__dirname, "optuna_dashboard/public"),
+        plugins: [
+            react(),
+            //splitVendorChunk()
+        ],
+    }
 });
